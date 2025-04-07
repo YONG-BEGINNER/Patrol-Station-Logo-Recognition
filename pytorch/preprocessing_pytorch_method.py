4f3cc6f59ac35e5b418868c70d80daea642b152b
Yong Yao Meng <yaomeng.yong@douyee.com>
2025-04-07 21:50:34 +0800

    Latest commit with 80% CNN Deploy

--- pytorch/preprocessing_pytorch_method.py
+++ pytorch/preprocessing_pytorch_method.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader, random_split
 from PIL import Image
 
 
-def load_data(data_path, transform, batch_size):
+def load_data(data_path, transform):
     dataset = datasets.ImageFolder(root=data_path, transform=transform)
     
     train_size = int(0.8* len(dataset))
@@ -14,8 +14,8 @@ def load_data(data_path, transform, batch_size):
     
     train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
     
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
     print("Class Labels: ", dataset.classes)
 
@@ -24,4 +24,4 @@ def load_data(data_path, transform, batch_size):
         print("Class Names:", [dataset.classes[i] for i in labels])  # Convert indices to class names
         break  # Only check one batch
     
-    return train_loader, test_dataset
+    return train_loader, test_dataset, dataset.classes
--- pytorch/pytorch_beginner.py
+++ pytorch/pytorch_beginner.py
@@ -1,10 +1,14 @@
 import os
 import cv2
 import math
+import torch
 import numpy as np
 from preprocessing_pytorch_method import load_data
+from CNN_Model import SimpleCNN
 from preprocessing_ml_method import train_test_split_func
 from torchvision import transforms, datasets
+import torch.optim as optim
+import torch.nn as nn
 
 data_path = "./Crop"
 print(os.path.abspath(data_path))
@@ -15,8 +19,35 @@ transform = transforms.Compose([
     transforms.Normalize(mean = [0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
 
-x, y = load_data(data_path, transform)
-x_train, x_test, y_train, y_test = train_test_split_func(x, y, 0.2, 42)
+train_data, test_data, classes = load_data(data_path, transform)
+
+num_classes = len(classes)
+model = SimpleCNN(num_classes)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(5):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for images, labels in train_data:
+        output = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = torch.max(outputs, 1)
+        correct +=(predicted == labels).sum().item()
+        total += labels.size(0)
+
+    print (f"Epoch {epoch+1}, Loss:{running_loss:.3f}, Accuracy:{100*correct/total:.2f}%") 
+
+
 
 
 
